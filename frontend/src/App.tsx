a81818ca500d4ef073c0328aa1c2d77c07c3e884
HVMS <viralsid2330@gmail.com>
2023-06-15 23:46:27 -0300

    App.tsx file upadated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+import { Box, Flex, ChakraProvider } from '@chakra-ui/react';
+import AssignmentModal from './components/AssignmentModal';
+import AssignmentList from './components/AssignmentList';
+import AssignmentText from './components/AssignmentText';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Assignment {
+  id: number;
+  title: string;
+  description: string;
+  visibleDate: string;
 }
+
+const App: React.FC = () => {
+  const [assignments, setAssignments] = useState<Assignment[]>([]);
+  const [nextAssignmentId, setNextAssignmentId] = useState(1);
+
+  const handleAssignmentSubmit = (assignment: Assignment) => {
+    const newAssignment: Assignment = {
+      ...assignment,
+      id: nextAssignmentId,
+    };
+
+    setAssignments([...assignments, newAssignment]);
+    setNextAssignmentId(nextAssignmentId + 1);
+  };
+
+  const handleAssignmentDelete = (id: number) => {
+    setAssignments(assignments.filter(assignment => assignment.id !== id));
+  };
+  
+  return (
+    <ChakraProvider>
+      <Box>
+        <Flex as="nav" justify="space-between" wrap="wrap" padding={4} bg="gray.800" color="white">
+          <AssignmentText></AssignmentText>
+          <AssignmentModal onSubmit={handleAssignmentSubmit} />
+        </Flex>
+      </Box>
+      <AssignmentList assignments={assignments} onDelete={handleAssignmentDelete} />
+    </ChakraProvider>
+  );
+};
 
 export default App;
