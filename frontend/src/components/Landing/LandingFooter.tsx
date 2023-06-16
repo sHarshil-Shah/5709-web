bc467f65acbe8c8f6a3879c67561e3092eddfa7e
HVMS <viralsid2330@gmail.com>
2023-06-16 19:21:19 -0300

    Footer page added

--- frontend/src/components/Landing/LandingFooter.tsx
+++ frontend/src/components/Landing/LandingFooter.tsx
@@ -1,44 +1,49 @@
 import React from "react";
-import { Box, Flex, Heading, Image } from "@chakra-ui/react";
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 const LandingFooter = () => {
-    return (
-        <Flex mt={8} justifyContent="center" flexWrap="wrap">
-            <Box
-                bg="white"
-                p={6}
-                borderRadius="md"
-                boxShadow="md"
-                maxWidth="400px"
-                width={{ base: "100%", sm: "50%" }}
-                mx={{ base: 0, sm: 2 }}
-                mb={4}
-                shadow={"lg"}
-            ></Box>
+  return (
+    <Flex
+      mt={8}
+      justifyContent="center"
+      flexWrap="wrap"
+      alignItems="stretch"
+    >
+      <Box
+        bg="white"
+        p={6}
+        borderRadius="md"
+        boxShadow="md"
+        flex="1"
+        maxWidth="400px"
+        width={{ base: "100%", sm: "50%" }}
+        mx={{ base: 0, sm: 2 }}
+        mb={4}
+        shadow={"lg"}
+      >
+      </Box>
 
-            <Box
-                width="2px"
-                height="200px"
-                bg="black"
-                mx={{ base: 0, sm: 2 }}
-                mb={4}
-            />
+      <Box
+        width="2px"
+        bg="black"
+        mx={{ base: 0, sm: 2 }}
+        mb={4}
+      />
 
-            <Box
-                bg="#white"
-                p={6}
-                borderRadius="md"
-                boxShadow="md"
-                maxWidth="400px"
-                width={{ base: "100%", sm: "50%" }}
-                mx={{ base: 0, sm: 2 }}
-                mb={4}
-                shadow={"lg"}
-            >
-            </Box>
-        </Flex>
-    )
-}
+      <Box
+        bg="#white"
+        p={6}
+        borderRadius="md"
+        boxShadow="md"
+        flex="1"
+        maxWidth="400px"
+        width={{ base: "100%", sm: "50%" }}
+        mx={{ base: 0, sm: 2 }}
+        mb={4}
+        shadow={"lg"}
+      ></Box>
+    </Flex>
+  );
+};
 
 export default LandingFooter;
