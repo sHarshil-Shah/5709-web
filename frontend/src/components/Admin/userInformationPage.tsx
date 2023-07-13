7350fe3ebfec16a91d13b9d7ffcbcee94665df15
Aanandi Pankhania <an936894@dal.ca>
2023-07-13 07:10:57 -0300

    [Feature- Admin Dashboard] Added User list from UserManagement to Admin Dashboard

--- frontend/src/components/Admin/userInformationPage.tsx
+++ frontend/src/components/Admin/userInformationPage.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Box, Flex, ChakraProvider, Text, Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
-
+import AdminNavBar from './adminNavigationBar';
+import TableWithFilters from '../UserManagement/listUsers';
 
 const UserInformationPage = () => {
   return (
     <ChakraProvider>
+      <Box>
+        <AdminNavBar />
+      </Box>
         <Box p={8}>
-        <Text fontSize="xl" fontWeight="bold" mb={4}>
-          Welcome to User Information Page!
-        </Text>
+        <TableWithFilters />
       </Box>
     </ChakraProvider>
   );
