0ce57dba9f84e5672b8714e985425192c45df14c
HVMS <viralsid2330@gmail.com>
2023-07-18 23:11:33 -0300

    Assignment List updated

--- frontend/src/components/Assignment/AssignmentList.tsx
+++ frontend/src/components/Assignment/AssignmentList.tsx
@@ -1,114 +1,7 @@
-// import React, {useState, useRef } from 'react';
-// import { Text, Table, Thead, Tbody, Tr, Th, Td, VStack, Card,
-//   AlertDialog,
-//   AlertDialogBody,
-//   AlertDialogFooter,
-//   AlertDialogHeader,
-//   AlertDialogContent,
-//   AlertDialogOverlay,
-//   Button
-// } from '@chakra-ui/react';
-
-// interface Assignment {
-//   id: number;
-//   title: string;
-//   description: string;
-//   visibleDate: string;
-// }
-
-// interface AssignmentListProps {
-//   assignments: Assignment[];
-//   onDelete: (id: number) => void;
-// }
-
-// const AssignmentList: React.FC<AssignmentListProps> = ({ assignments, onDelete }) => {
-
-//   const leastDestructiveRef = useRef<HTMLButtonElement | null>(null);
-//   const [selectedAssignment, setSelectedAssignment] = useState<Assignment | null>(null);
-//   const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
-
-//   const handleDelete = (assignment: Assignment) => {
-//     setSelectedAssignment(assignment);
-//     setIsConfirmationOpen(true);
-//   };
-
-//   const handleConfirmationClose = () => {
-//     setSelectedAssignment(null);
-//     setIsConfirmationOpen(false);
-//   };
-
-//   const handleConfirmationConfirm = () => {
-//     if (selectedAssignment) {
-//       // Perform the delete operation here
-//       console.log('Deleting assignment:', selectedAssignment);
-//     }
-//     handleConfirmationClose();
-//   };
-
-//   return (
-//     <VStack spacing={4} align="stretch">
-//       <Text fontSize={25} fontWeight="bold" margin={5}>Assignment List:</Text>
-//       <Card borderRadius="5px" marginLeft={5} marginRight={5}>
-//         <Table variant="simple">
-//           <Thead>
-//             <Tr>
-//               <Th textAlign="center">Title</Th>
-//               <Th textAlign="center">Description</Th>
-//               <Th textAlign="center">Visible Date</Th>
-//               <Th textAlign="center">Action</Th>
-//             </Tr>
-//           </Thead>
-//           <Tbody>
-//             {assignments.map((assignment, index) => (
-//               <Tr key={index}>
-//                 <Td textAlign="center">{assignment.title}</Td>
-//                 <Td textAlign="center">{assignment.description}</Td>
-//                 <Td textAlign="center">{assignment.visibleDate}</Td>
-//                 <Td textAlign="center">
-//                     <Button colorScheme="red" size="sm" onClick={() => handleDelete(assignment)}>
-//                       Delete
-//                     </Button>
-//                   </Td>
-//               </Tr>
-//             ))}
-//           </Tbody>
-//         </Table>
-//       </Card>
-//       <AlertDialog
-//         isOpen={isConfirmationOpen}
-//         leastDestructiveRef={leastDestructiveRef}
-//         onClose={handleConfirmationClose}
-//       >
-//         <AlertDialogOverlay>
-//           <AlertDialogContent>
-//             <AlertDialogHeader fontSize="lg" fontWeight="bold">
-//               Delete Assignment
-//             </AlertDialogHeader>
-
-//             <AlertDialogBody>
-//               Are you sure you want to delete this assignment?
-//             </AlertDialogBody>
-
-//             <AlertDialogFooter>
-//               <Button colorScheme="red" onClick={handleConfirmationConfirm}>
-//                 Yes
-//               </Button>
-//               <Button colorScheme="gray" onClick={handleConfirmationClose} ml={3}>
-//                 No
-//               </Button>
-//             </AlertDialogFooter>
-//           </AlertDialogContent>
-//         </AlertDialogOverlay>
-//       </AlertDialog>
-//     </VStack>
-//   );
-// };
-
-// export default AssignmentList;
-
 import React, { useState, useRef } from 'react';
 import {
   Text,
+  HStack,
   Table,
   Thead,
   Tbody,
@@ -124,6 +17,7 @@ import {
   AlertDialogContent,
   AlertDialogOverlay,
   Button,
+  Center,
 } from '@chakra-ui/react';
 
 interface Assignment {
@@ -183,11 +77,19 @@ const AssignmentList: React.FC = () => {
     handleConfirmationClose();
   };
 
+  const handleEdit = (assignment: Assignment) => {
+    // Logic for handling the edit operation
+    console.log('Editing assignment:', assignment);
+    // You can open the assignment modal and populate it with the assignment data here
+    // For simplicity, let's just log the assignment for now
+    setSelectedAssignment(assignment);
+  };
+
   return (
     <VStack spacing={4} align="stretch">
-      <Text fontSize={25} fontWeight="bold" margin={5}>
+      {/* <Text fontSize={25} fontWeight="bold" margin={5}>
         Assignment List:
-      </Text>
+      </Text> */}
       <Card borderRadius="5px" mx={5}>
         <Table variant="simple">
           <Thead>
@@ -204,14 +106,23 @@ const AssignmentList: React.FC = () => {
                 <Td textAlign="center">{assignment.title}</Td>
                 <Td textAlign="center">{assignment.description}</Td>
                 <Td textAlign="center">{assignment.visibleDate}</Td>
-                <Td textAlign="center">
-                  <Button
-                    colorScheme="red"
-                    size="sm"
-                    onClick={() => handleDelete(assignment)}
-                  >
-                    Delete
-                  </Button>
+                <Td textAlign="left">
+                  <HStack spacing={2} justifyContent={'center'}>
+                      <Button
+                        colorScheme="blue"
+                        size="sm"
+                        onClick={() => handleEdit(assignment)}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        colorScheme="red"
+                        size="sm"
+                        onClick={() => handleDelete(assignment)}
+                      >
+                        Delete
+                      </Button>
+                    </HStack>
                 </Td>
               </Tr>
             ))}
