24bd2667c8595682d7a0e1c451ace78917be4728
Aanandi Pankhania <an936894@dal.ca>
2023-07-26 03:56:46 -0300

    mapping feature

--- frontend/src/components/Admin/pendingApproval.tsx
+++ frontend/src/components/Admin/pendingApproval.tsx
@@ -1,21 +1,116 @@
-import React from 'react';
-import { Box, Flex, ChakraProvider, Text, Link } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  Box,
+  ChakraProvider,
+  Text,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '@chakra-ui/react';
 import AdminNavBar from './adminNavigationBar';
+import { Accordion } from 'react-bootstrap';
 
 const PendingApproval = () => {
-    return (
-        <ChakraProvider>
-            <Box>
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
+
+  return (
+    <ChakraProvider>
+      <Box>
         <AdminNavBar />
       </Box>
-            <Box p={8}>
-            <Text fontSize="xl" fontWeight="bold" mb={4}>
-              Welcome to Pending Approval!
-            </Text>
-          </Box>
-        </ChakraProvider>
-      );
+      <Box p={8}>
+        <Text fontSize="xl" fontWeight="bold" mb={4}>
+          Welcome to Pending Approval!
+        </Text>
+
+        <div style={{ marginLeft: '8%', marginBottom: '3%', marginRight: '8%' }}>
+        <Accordion defaultActiveKey={['0']} alwaysOpen>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Request ID: #1</Accordion.Header>
+          <Accordion.Body>
+            <div className="d-flex flex-column align-items-start">
+              <img src="https://picsum.photos/400" alt="Profile" style={{ width: '100px', height: '100px' }} />
+              <h4>Name</h4>
+              <p>Birthdate: MM/DD/YYYY</p>
+              <p>Age: XX</p>
+              <p>Field of Specialization: Field</p>
+              <p>Education: Education Details</p>
+              <a href="link-url">Link</a>
+              <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div className="d-flex align-items-start">
+              <Button variant="success" onClick={handleShow}>Approve</Button>
+              <Button variant="danger" className="ms-3" onClick={handleShow2}>Reject</Button>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1" style={{marginTop: '3%'}}>
+          <Accordion.Header>Request ID: #2</Accordion.Header>
+          <Accordion.Body>
+          <div className="d-flex flex-column align-items-start">
+              <img src="https://picsum.photos/400" alt="Profile" style={{ width: '100px', height: '100px' }} />
+              <h4>Name</h4>
+              <p>Birthdate: MM/DD/YYYY</p>
+              <p>Age: XX</p>
+              <p>Field of Specialization: Field</p>
+              <p>Education: Education Details</p>
+              <a href="link-url">Link</a>
+              <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div className="d-flex align-items-start">
+              <Button variant="success" onClick={handleShow}>Approve</Button>
+              <Button variant="danger" className="ms-3" onClick={handleShow2}>Reject</Button>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+        </div>
+
+        <Modal isOpen={show} onClose={handleClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Are you sure?</ModalHeader>
+            <ModalBody>You are trying to approve a professor's application.</ModalBody>
+            <ModalFooter>
+              <Button variant="ghost" onClick={handleClose}>
+                Close
+              </Button>
+              <Button colorScheme="green" onClick={handleClose} ml={3}>
+                Confirm
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        <Modal isOpen={show2} onClose={handleClose2}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Are you sure?</ModalHeader>
+            <ModalBody>You are trying to reject a professor's application.</ModalBody>
+            <ModalFooter>
+              <Button variant="ghost" onClick={handleClose2}>
+                Close
+              </Button>
+              <Button colorScheme="red" onClick={handleClose2} ml={3}>
+                Reject
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
+    </ChakraProvider>
+  );
 };
 
 export default PendingApproval;
--- frontend/src/components/Admin/professorMapping.tsx
+++ frontend/src/components/Admin/professorMapping.tsx
@@ -1,18 +1,72 @@
-import React from 'react';
-import { Box, Flex, ChakraProvider, Text, Link } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Box, ChakraProvider, Text, Button, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter } from '@chakra-ui/react';
 import AdminNavBar from './adminNavigationBar';
+import { Card, Dropdown, DropdownButton, ListGroup,Modal } from 'react-bootstrap';
 
 const ProfessorMapping = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [selectedOption, setSelectedOption] = useState('Select an Instructor');
+  const handleOptionSelect = (option: string) => {
+    setSelectedOption(option);
+  };
+
   return (
     <ChakraProvider>
-        <Box>
+      <Box>
         <AdminNavBar />
       </Box>
-        <Box p={8}>
+      <Box p={8}>
         <Text fontSize="xl" fontWeight="bold" mb={4}>
           Welcome to Professor Mapping Section!
         </Text>
+        {/* Use the MappingCard component here */}
+        <div className="d-flex flex-column align-items-start" style={{ marginLeft: '8%', marginRight: '8%', marginTop: '3%', marginBottom: '8%' }}>
+          <div style={{ fontSize: '32px', fontWeight: 700 }}>Mapping</div>
+          <div className="d-flex  flex-wrap justify-content-between" style={{ width: '100%' }}>
+            <div className="mt-5">
+              <div>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Body>
+                    <Card.Title>Course Title</Card.Title>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Id: Course ID</ListGroup.Item>
+                    <ListGroup.Item>Course Name: Course Name</ListGroup.Item>
+                  </ListGroup>
+                  <Card.Body>
+                    <DropdownButton variant="primary" title={selectedOption}>
+                      <Dropdown.Item onClick={() => handleOptionSelect('Dr. Dey')}>Dr. Dey</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleOptionSelect('Dr. Sharma')}>Dr. Sharma</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleOptionSelect('Dr. darshana')}>Dr. darshana</Dropdown.Item>
+                    </DropdownButton>
+                    <Button variant="primary" onClick={handleShow} style={{ marginTop: '10px' }}>
+                      Allocate
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
+
+              <Modal show ={show} onHide={handleClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Are you sure?</ModalHeader>
+                  <ModalBody>You are trying to map a professor to a course!</ModalBody>
+                  <ModalFooter>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    <Button variant="success" onClick={handleClose}>
+                      Confirm
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            </div>
+          </div>
+        </div>
       </Box>
     </ChakraProvider>
   );
