dfe804dfe7e5e11a1e16fff2cc3fdc9982d70c9a
Harshil <sharshil1299@gmail.com>
2023-07-26 08:04:13 -0300

    Forget password redirect added in landing page

--- frontend/src/components/Landing/LandingPage.tsx
+++ frontend/src/components/Landing/LandingPage.tsx
@@ -1,20 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import {Box} from "@chakra-ui/react";
 import LandingPageHeader from "./LandingPageHeader";
 import LandingCardViews from "./LandingCardViews";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
 
 const LandingPage = () => {
-  return (
-    <Box p={10}>
 
-      <Box textAlign="left">
-        <LandingPageHeader></LandingPageHeader>
-      </Box>
+    const navigate = useNavigate();
 
-      <Box textAlign="center">
-        <LandingCardViews></LandingCardViews>
-      </Box>
-    </Box>
-  );
+    const location = useLocation();
+
+    useEffect(() => {
+
+        // Parse the query parameters from the location object
+        const queryParams = new URLSearchParams(location.search);
+        const feature = queryParams.get("feature");
+
+        if (feature === "forgetPassword") {
+            navigate('/forgetPassword?user_id=' + queryParams.get("user_id"));
+        }
+    });
+    return (
+        <Box p={10}>
+
+            <Box textAlign="left">
+                <LandingPageHeader></LandingPageHeader>
+            </Box>
+
+            <Box textAlign="center">
+                <LandingCardViews></LandingCardViews>
+            </Box>
+        </Box>
+    );
 };
 
 export default LandingPage;
